0bc388c13bd83b198282d2e437c74b26611e25aa
Marco Bernasocchi <marco@opengis.ch>
2016-09-16 22:35:59 +0200

    new style init works

--- __init__.py
+++ __init__.py
@@ -35,22 +35,23 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-
+    plugin_name = os.path.dirname(__file__).split(os.path.sep)[-1]
+    plugin_name = qgis.utils.pluginMetadata(plugin_name, 'name')
     try:
         # qgis.PyQt is available in QGIS >=2.14
         from qgis.PyQt.QtCore import qVersion
         # qgis.utils.QGis is available in QGIS < 3
         if hasattr(qgis.utils, 'QGis'):
             import qgis2compat.apicompat
+            qgis2compat.log('apicompat used in %s' % plugin_name)
     except ImportError:
         try:
             # we are in QGIS < 2.14
             import qgis2compat
             import qgis2compat.apicompat
+            qgis2compat.log('PyQt and apicompat used in %s' % plugin_name)
         except ImportError:
             import traceback
-            plugin_name = os.path.dirname(__file__).split(os.path.sep)[-1]
-            plugin_name = qgis.utils.pluginMetadata(plugin_name, 'name')
             message = ('The Plugin %s uses the QGIS2compat plugin. '
                        'Please install it with the plugin manager it and '
                        'restart QGIS. For more information read '
@@ -59,7 +60,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
             traceback.print_exc()
             raise ImportError(message)
 
-    from qgis.PyQt.QtCore import qVersion
-    print qVersion()
     from .veriso import VeriSO
     return VeriSO(iface)
