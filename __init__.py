7279bf45679ca398605163b918354cab8b0e5a05
Marco Bernasocchi <marco@opengis.ch>
2016-11-10 17:19:10 +0100

    make unittests use qgis2compat plugin

--- __init__.py
+++ __init__.py
@@ -24,11 +24,10 @@
 """
 
 
-import os
-import qgis.utils
+from base.utils.utils import check_compat
+check_compat()
 
 
-# noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load VeriSO class from file VeriSO.
 
@@ -36,30 +35,7 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
 
-    plugin_name = os.path.dirname(__file__).split(os.path.sep)[-1]
-    plugin_name = qgis.utils.pluginMetadata(plugin_name, 'name')
-    try:
-        # qgis.PyQt is available in QGIS >=2.14
-        from qgis.PyQt.QtCore import qVersion
-        # qgis.utils.QGis is available in QGIS < 3
-        if hasattr(qgis.utils, 'QGis'):
-            import qgis2compat.apicompat
-            qgis2compat.log('apicompat used in %s' % plugin_name)
-    except ImportError:
-        try:
-            # we are in QGIS < 2.14
-            import qgis2compat
-            import qgis2compat.apicompat
-            qgis2compat.log('PyQt and apicompat used in %s' % plugin_name)
-        except ImportError:
-            import traceback
-            message = ('The Plugin %s uses the QGIS2compat plugin. '
-                       'Please install it with the plugin manager it and '
-                       'restart QGIS. For more information read '
-                       'http://opengis.ch/qgis2compat' %
-                       plugin_name)
-            traceback.print_exc()
-            raise ImportError(message)
-
     from .veriso import VeriSO
     return VeriSO(iface)
+
+
