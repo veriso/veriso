2e1647328fbc51efcd4d5282d6dadb394c11c1e5
Marco Bernasocchi <marco@opengis.ch>
2017-01-08 16:28:21 +0100

    moved example to template applicationmodule

--- templates/template_module/applicationmodule.py
+++ templates/template_module/applicationmodule.py
@@ -11,3 +11,28 @@ class ApplicationModule(ApplicationModuleBase):
     """
     def __init__(self, iface, toolbar, locale_path):
         super(ApplicationModule, self).__init__(iface, toolbar, locale_path)
+
+"""
+    def do_load_defects(self):
+        # example of how to add your own defect layers or fields
+        defects_module = 'veriso.modules.loaddefects_base'
+        defects_module = dynamic_import(defects_module)
+        d = defects_module.LoadDefectsBase(self.iface, self.module,
+                                           self.module_name)
+
+        d.layers['point']['fields'] = {
+            'ogc_fid': {'widget': 'TextEdit'},
+            'topic': {
+                'widget': 'Enumeration',
+                'alias': 'Topic:'
+            },
+            'bemerkung': {
+                'widget': 'TextEdit',
+                'alias': 'Bemekung:',
+                'config': {"IsMultiline": True}
+            },
+            'datum': {'widget': 'Hidden'}
+        }
+
+        d.run()
+"""
