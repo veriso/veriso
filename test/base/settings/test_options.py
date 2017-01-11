b97d4430a6c61392a4e8f1fd574461b73112f189
Mario Baranzini <mario.baranzini@gmail.com>
2017-01-12 00:11:21 +0100

    Update test settings options

--- test/base/settings/test_options.py
+++ test/base/settings/test_options.py
@@ -1,15 +1,20 @@
+from __future__ import absolute_import
+
 import os
 
 from unittest import TestCase
 
 try:
-    import mock
+    from mock import patch, Mock
 except ImportError:
-    from unittest import mock
+    from unittest.mock import patch, Mock
 
+import qgis
 from qgis.PyQt.QtCore import QSettings
 from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtCore import QTimer
 from qgis.PyQt.QtTest import QTest
+from qgis.PyQt.QtWidgets import QFileDialog
 
 from veriso.test.utilities import get_qgis_app
 from veriso.base.settings.options import OptionsDialog
@@ -58,8 +63,41 @@ class TestUtils(TestCase):
         self.assertEquals(lst_items[0], 'http://www.catais.org/models/')
         self.assertEquals(lst_items[1], 'http://models.geo.admin.ch/')
 
-    def test_on_on_btnBrowseImportJar_clicked(self):
+    @patch('qgis.PyQt.QtWidgets.QFileDialog.getOpenFileName')
+    def test_on_btnBrowseImportJar_clicked(self, mock_on_btnBrowseImportJar_clicked):
         '''test Button Browse Import Jar'''
-        #button = self.dialog.btnBrowseImportJar
-        #@mock.patch('test_options.QFileDialog')
-        pass
+        mock_on_btnBrowseImportJar_clicked.return_value = "/foo/bar.jar"
+        button = self.dialog.btnBrowseImportJar
+        button.click()
+        self.assertEquals(self.dialog.lineEditImportJar.text(), "/foo/bar.jar")
+
+    @patch('qgis.PyQt.QtWidgets.QFileDialog.getOpenFileName')
+    def test_on_btnBrowseProjectsDatabase_clicked(self, mock_on_btnBrowseProjectsDatabase_clicked):
+        '''test Button Browse Project Database'''
+        mock_on_btnBrowseProjectsDatabase_clicked.return_value = "/foo/bar.db"
+        button = self.dialog.btnBrowseProjectsDatabase
+        button.click()
+        self.assertEquals(self.dialog.lineEditProjectsDatabase.text(), "/foo/bar.db")
+
+    @patch('qgis.PyQt.QtWidgets.QFileDialog.getExistingDirectory')
+    def test_on_btnBrowseProjectsRootDir_clicked(self, mock_on_btnBrowseProjectsRootDir_clicked):
+        '''test Button Project Root Dir'''
+        mock_on_btnBrowseProjectsRootDir_clicked.return_value = "/foo/bar/"
+        button = self.dialog.btnBrowseProjectsRootDir
+        button.click()
+        self.assertEquals(self.dialog.lineEditProjectsRootDir.text(), "/foo/bar/")
+
+    #ToDo Marioba
+    # @patch('veriso.base.settings.options.OptionsDialog.test_connection_succes')
+    # @patch('veriso.base.settings.options.OptionsDialog.test_connection_failed')
+    # def test_on_btnTestProjectDB_clicked(self, mock_test_connection_failed, mock_test_connection_succes):
+    #     '''test Button Test Project DB'''
+    #     self.dialog.lineEditProjectsDatabase.setText("")
+    #     button = self.dialog.btnTestProjectDB
+    #     button.click()
+    #     assert mock_test_connection_failed.called
+    #     mock_test_connection_failed.called_once.reset_mock
+    #     self.dialog.lineEditProjectsDatabase.setText("/home/mario/.qgis2/python/plugins/veriso/templates/template_projects.db")
+    #     button.click()
+    #     assert mock_test_connection_succes.called
+
