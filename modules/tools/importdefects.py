6aea46b3861ef7f309167feeb70c674b52f1ceb5
Mario Baranzini <mario@opengis.ch>
2017-05-02 14:47:57 +0200

    import_defects corrections

--- modules/tools/importdefects.py
+++ modules/tools/importdefects.py
@@ -3,6 +3,7 @@ import os
 import sys
 import traceback
 
+
 from builtins import range, str
 
 from openpyxl import load_workbook
@@ -27,6 +28,7 @@ from veriso.base.utils.utils import (open_psql_db, open_sqlite_db,
                                      db_user_has_role, get_absolute_path)
 
 from veriso.base.utils.utils import dynamic_import
+from veriso.base.utils.exceptions import VerisoErrorWithBar
 
 FORM_CLASS = get_ui_class('importdefects.ui')
 
@@ -64,11 +66,6 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
         xlsx = self.lineEditDefectsFile.text().strip()
         load_checked = self.load_defects_check.isChecked()
 
-        header_list_punkte, rows_list_punkte = '', ''
-        header_list_linien, rows_list_linien = '', ''
-        header_list_polygone, rows_list_polygone = '', ''
-
-
         if xlsx == '':
             self.message_bar.pushWarning("VeriSO",
                                          tr("No Defects file set."))
@@ -76,45 +73,31 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
 
         self.wb = load_workbook(filename = xlsx, read_only = True)
 
-        try:
-            header_list_punkte, rows_list_punkte = self.read_sheet(u'Mängelliste (Punkte)')
-            header_list_linien, rows_list_linien = self.read_sheet(u'Mängelliste (Linien)')
-            header_list_polygone, rows_list_polygone = self.read_sheet(u'Mängelliste (Polygone)')
-        except WrongExcelError as e:
-            self.iface.messageBar().pushMessage('Wrong Excel file', e.value,
-                                                level=QgsMessageBar.CRITICAL, duration=5)
-            QApplication.restoreOverrideCursor()
-            return
+        header_list_points, rows_list_points = self.read_sheet(u'Mängelliste (Punkte)')
+        header_list_lines, rows_list_lines = self.read_sheet(u'Mängelliste (Linien)')
+        header_list_polygons, rows_list_polygons = self.read_sheet(u'Mängelliste (Polygone)')
 
-        punkte_query = self.create_query('t_maengel_punkt', header_list_punkte, rows_list_punkte)
-        linien_query = self.create_query('t_maengel_linie', header_list_linien, rows_list_linien)
-        polygone_query = self.create_query('t_maengel_polygon', header_list_polygone, rows_list_polygone)
+        self.open_db()
 
-        try:
-            self.open_db()
+        if len(rows_list_points) > 0:
+            query_points = self.create_query('t_maengel_punkt', header_list_points, rows_list_points)
+            self.execute_query(query_points)
+        if len(rows_list_lines) > 0:
+            query_lines = self.create_query('t_maengel_linie', header_list_lines, rows_list_lines)
+            self.execute_query(query_lines)
+        if len(rows_list_polygons) > 0:
+            query_polygons = self.create_query('t_maengel_polygon', header_list_polygons, rows_list_polygons)
+            self.execute_query(query_polygons)
 
-            self.execute_query(punkte_query)
-            self.execute_query(linien_query)
-            self.execute_query(polygone_query)
+        self.db.close()
+        self.iface.messageBar().pushInfo("VeriSo", "Defects imported")
 
-            self.db.close()
-            self.iface.messageBar().pushInfo("VeriSo", "Defects imported")
-
-            if load_checked:
-                defects_module = 'veriso.modules.loaddefects_base'
-                defects_module = dynamic_import(defects_module)
-                d = defects_module.LoadDefectsBase(self.iface, self.module_name)
-                defects_layers = d.run()
-                self.defects_list_dock.load_layers(defects_layers)
-
-            QApplication.restoreOverrideCursor()
-
-        except Exception:
-            QApplication.restoreOverrideCursor()
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            self.iface.messageBar().pushMessage("Error",
-                str(traceback.format_exc(exc_traceback)), level=QgsMessageBar.CRITICAL, duration=5)
-            return
+        if load_checked:
+            defects_module = 'veriso.modules.loaddefects_base'
+            defects_module = dynamic_import(defects_module)
+            d = defects_module.LoadDefectsBase(self.iface, self.module_name)
+            defects_layers = d.run()
+            self.defects_list_dock.load_layers(defects_layers)
 
         QApplication.restoreOverrideCursor()
 
@@ -123,13 +106,15 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
     def read_sheet(self, sheet_name):
         sheet = self.wb[sheet_name]
 
-        # list of dictionaries. A dictionary per row with key = column header
+        # list of lists. A list per row with key = column header
         rows_list = []
         header_list = []
 
         # check if the cells are in the original positions
-        if(sheet['A5'].value != 'ogc_fid'):
-             raise WrongExcelError('Wrong Excel file, cell A5 is not equal to \'ogc_fid\'')
+        if sheet['A5'].value != 'ogc_fid':
+            QApplication.restoreOverrideCursor()
+            e = 'Wrong Excel file, cell A5 is not \'ogc_fid\''
+            raise VerisoErrorWithBar(self.iface.messageBar(), e)
         else:
             # get the column names (row 5)
             for cell in sheet[5]:
@@ -155,7 +140,6 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
         query += '), (\''.join(['\', \''.join([str(value) for value in row[1:11]])
                                 + '\', ST_GeomFromText(\''+row[-1]+'\', 2056)' for row in rows_list])
         query += ')'
-
         return query
 
     def open_db(self):
@@ -170,7 +154,7 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
             self.db.open()
 
         except Exception as e:
-            raise e
+            raise VerisoErrorWithBar(self.iface.messageBar(), "Error " + e.message)
 
     def execute_query(self, sql):
         query = QSqlQuery(self.db)
@@ -178,7 +162,8 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
         res = query.exec_(sql)
 
         if(res == False):
-            raise QueryExecutionError(query.lastError().text())
+            QApplication.restoreOverrideCursor()
+            raise VerisoErrorWithBar(self.iface.messageBar(), "Error " + (query.lastError().text()))
 
     # noinspection PyPep8Naming,PyPep8Naming
     @pyqtSignature("on_btnBrowseDefectsFile_clicked()")
@@ -190,16 +175,4 @@ class ImportDefectsDialog(QDialog, FORM_CLASS):
                 "XLSX (*.xlsx)")
         file_info = QFileInfo(file_path)
         self.lineEditDefectsFile.setText(file_info.absoluteFilePath())
-
-class QueryExecutionError(Exception):
-    def __init__(self, value):
-        self.value = value
-        def __str__(self):
-            return repr(self.value)
-
-class WrongExcelError(Exception):
-    def __init__(self, value):
-        self.value = value
-        def __str__(self):
-            return repr(self.value)
 
